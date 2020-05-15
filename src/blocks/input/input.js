0f4a0fc8e93336fe41045da849db0b2d349dfacc
Michael <ko4erov@mail.ru>
2020-05-15 20:35:10 +0300

    add button to load file input

--- src/blocks/input/input.js
+++ src/blocks/input/input.js
@@ -1,1 +1,16 @@
 import './input.scss'
+
+
+showLoadedFile()
+// показываем в инпуте название выбранного файла
+function showLoadedFile() {
+	document.querySelectorAll('input[type="file"]').forEach((input)=> {
+		input.addEventListener('change', function () {
+			if (this.files[0]) {
+				this.closest('.load-input').querySelector('.load-input__text').textContent = this.files[0].name
+			} else {
+				this.closest('.load-input').querySelector('.load-input__text').textContent = 'Загрузить изображение'
+			}
+		})
+	})
+}
