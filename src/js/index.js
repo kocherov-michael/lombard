d22970f96f78f0e409ba8c822daef82b8d0de8de
Michael <ko4erov@mail.ru>
2020-05-16 18:31:55 +0300

    jewelry-page ready

--- src/js/index.js
+++ src/js/index.js
@@ -72,3 +72,27 @@ $(document).ready(function() {
         }
     });
 });
+
+$(document).ready(function() {
+    jQuery('#carouselAdvantage').owlCarousel({
+        items: 8, //Кол-во отображаемых элементов
+        loop:false, //Зацикливаем слайдер
+        margin:50, //Отступ от элемента справа в 50px
+        dots:true,
+        nav:true, //Отключение навигации
+        autoplay:false, //Автозапуск слайдера
+        smartSpeed:1000, //Время движения слайда
+        autoplayTimeout:2000, //Время смены слайда
+        responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+            0:{
+                items:1
+            },
+            // 600:{
+            //     items:2
+            // },
+            768:{
+                items:8
+            }
+        }
+    });
+});
