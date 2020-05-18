458babccd0fdeb24fc4a597330cb879827a2855c
Michael <ko4erov@mail.ru>
2020-05-18 18:34:52 +0300

    require fancy box

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import 'owl.carousel/dist/assets/owl.carousel.css';
 // import 'owl.carousel/dist/owl.carousel.js';
 import 'owl.carousel';
+require("@fancyapps/fancybox");
+require('@fancyapps/fancybox/dist/jquery.fancybox.css');
 
 import './../blocks/header/header.js'
 import './../blocks/logo/logo.js'
@@ -20,6 +22,7 @@ import './../blocks/dropdown-desc/dropdown-desc.js'
 import './../blocks/calc-item/calc-item.js'
 import './../blocks/info-main-block/info-main-block.js'
 import './../blocks/block-accept/block-accept.js'
+import './../blocks/img-card/img-card.js'
 // import './../blocks/steps/steps.js'
 import './../blocks/map/map.js'
 import '../blocks/contact-card/contact-card.js'
@@ -27,76 +30,10 @@ import './../blocks/conditions/conditions.js'
 import './../blocks/conditions-card/conditions-card.js'
 import './../blocks/footer/footer.js'
 
-// $(document).ready(function() {
-//     jQuery('#carouselSteps').owlCarousel({
-//         items: 3, //Кол-во отображаемых элементов
-//         loop:false, //Зацикливаем слайдер
-//         margin:50, //Отступ от элемента справа в 50px
-//         dots:true,
-//         nav:true, //Отключение навигации
-//         autoplay:false, //Автозапуск слайдера
-//         smartSpeed:1000, //Время движения слайда
-//         autoplayTimeout:2000, //Время смены слайда
-//         responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
-//             0:{
-//                 items:1
-//             },
-//             // 600:{
-//             //     items:2
-//             // },
-//             768:{
-//                 items:3
-//             }
-//         }
-//     });
-// });
 
-// $(document).ready(function() {
-//     jQuery('#carouselExamples').owlCarousel({
-//         items: 3, //Кол-во отображаемых элементов
-//         loop:false, //Зацикливаем слайдер
-//         margin:50, //Отступ от элемента справа в 50px
-//         dots:true,
-//         nav:true, //Отключение навигации
-//         autoplay:false, //Автозапуск слайдера
-//         smartSpeed:1000, //Время движения слайда
-//         autoplayTimeout:2000, //Время смены слайда
-//         responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
-//             0:{
-//                 items:1
-//             },
-//             // 600:{
-//             //     items:2
-//             // },
-//             768:{
-//                 items:3
-//             }
-//         }
-//     });
-// });
 
 $(document).ready(function() {
-    // jQuery('#carouselAdvantage').owlCarousel({
-    //     items: 8, //Кол-во отображаемых элементов
-    //     loop:false, //Зацикливаем слайдер
-    //     margin:50, //Отступ от элемента справа в 50px
-    //     dots:true,
-    //     nav:true, //Отключение навигации
-    //     autoplay:false, //Автозапуск слайдера
-    //     smartSpeed:1000, //Время движения слайда
-    //     autoplayTimeout:2000, //Время смены слайда
-    //     responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
-    //         0:{
-    //             items:1
-    //         },
-    //         // 600:{
-    //         //     items:2
-    //         // },
-    //         768:{
-    //             items:8
-    //         }
-    //     }
-    // });
+    // console.log(jQuery)
     carouselHandler('data-owl-carousel')
 
     function carouselHandler(attr) {
@@ -139,9 +76,9 @@ $(document).ready(function() {
     // function owlCarouselCustom(attr) 
 });
 
-const linksElem = document.querySelectorAll('a')
-linksElem.forEach(link => {
-    link.addEventListener('click', () => {
-        console.log('click')
-    })
-})
+// const linksElem = document.querySelectorAll('a')
+// linksElem.forEach(link => {
+//     link.addEventListener('click', () => {
+//         console.log('click')
+//     })
+// })
