567f4c9dbadb95c58af0451d54e3bce041697c83
Michael <ko4erov@mail.ru>
2020-06-03 12:43:16 +0300

    refresh dist

--- src/blocks/map/map.js
+++ src/blocks/map/map.js
@@ -10,7 +10,7 @@ if (mapElement) {
 
     // Функция ymaps.ready() будет вызвана, когда
     // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-    // ymaps.ready(init);
+    ymaps.ready(init);
     function init(){
         // Создание карты.
         var myMap = new ymaps.Map("map", {
