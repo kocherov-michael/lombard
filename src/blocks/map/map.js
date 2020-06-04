55e3d752fb97899fd48c9be764ac51c1ebf82aba
Michael <ko4erov@mail.ru>
2020-06-04 18:23:33 +0300

    contact page for 1 address ready

--- src/blocks/map/map.js
+++ src/blocks/map/map.js
@@ -4,21 +4,37 @@ const mapElement = document.querySelector("#map")
 // console.log(mapElement)
 if (mapElement) {
 
-    
+    const contactsArray = []
 
+    const contactCardElements = document.querySelectorAll('[data-contact-card]')
+    contactCardElements.forEach(card => {
+        const x = card.getAttribute('data-coordinatesX')
+        const y = card.getAttribute('data-coordinatesY')
+        const title = card.getAttribute('data-coordinates-title')
+        // заполняем масив координат отделений
+        contactsArray.push({ x, y, title })
+
+    })
 
 
     // Функция ymaps.ready() будет вызвана, когда
     // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
     ymaps.ready(init);
     function init(){
+
+        const centerCoordinates = [59.955486, 30.337817]
+        // если только 1 точка, то пеомещаем её в центр
+        if (contactsArray.length === 1) {
+            centerCoordinates[0] = contactsArray[0].x
+            centerCoordinates[1] = contactsArray[0].y
+        }
         // Создание карты.
         var myMap = new ymaps.Map("map", {
             // Координаты центра карты.
             // Порядок по умолчанию: «широта, долгота».
             // Чтобы не определять координаты центра карты вручную,
             // воспользуйтесь инструментом Определение координат.
-            center: [59.955486, 30.337817],
+            center: centerCoordinates,
             // Уровень масштабирования. Допустимые значения:
             // от 0 (весь мир) до 19.
             zoom: 11,
@@ -56,6 +72,25 @@ if (mapElement) {
                     iconCaptionMaxWidth: '250'
                 }))
         })
+
+        if (contactsArray.length > 0) {
+            // обходим массив координат, которые создали перед инициализацией
+            contactsArray.forEach(card => {
+                // const x = card.getAttribute('data-coordinatesX')
+                // const y = card.getAttribute('data-coordinatesY')
+                // const title = card.getAttribute('data-coordinates-title')
+    
+                myMap.geoObjects
+                    .add(new ymaps.Placemark([card.x, card.y], {
+                        balloonContent: 'цвет <strong>голубой</strong>',
+                        iconCaption: card.title
+                    }, {
+                        preset: 'islands#blueCircleDotIconWithCaption',
+                        iconCaptionMaxWidth: '250'
+                    }))
+            })
+
+        }
 
     }
 }
