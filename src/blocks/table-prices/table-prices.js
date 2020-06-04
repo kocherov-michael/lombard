916e0c726399310381a41a1cd769afd6969f2fb7
Michael <ko4erov@mail.ru>
2020-06-04 13:20:55 +0300

    add interactive to table-item

--- src/blocks/table-prices/table-prices.js
+++ src/blocks/table-prices/table-prices.js
@@ -13,9 +13,12 @@ function chancgeTableCategories() {
                 if (categoryNumberElement.classList.contains('table-prices-head__blocks-item--active')) return
                 // получаем номер категории
                 const categoryNumber = event.target.getAttribute('data-table-price')
-                console.log(categoryNumber)
+                
                 // обёртка текстовых блоков описания
                 const descWrapper = table.querySelector('[data-table-desc-wrapper]')
+                // обёртка цен в table-item
+                const priceWrapperList = table.querySelectorAll('[data-table-price-wrapper]')
+                // console.log(priceWrapper)
                 let exActiveNumber
 
                 // удаляем активный класс у активного элемента
@@ -27,9 +30,15 @@ function chancgeTableCategories() {
                 })
                 // даём активный класс кликнотому элементу
                 event.target.classList.add('table-prices-head__blocks-item--active')
+                // передвигаем текст в описании
                 descWrapper.classList.remove(`table-prices-head__desc-wrapper--${exActiveNumber}`)
                 descWrapper.classList.add(`table-prices-head__desc-wrapper--${categoryNumber}`)
-
+                
+                // передвигаем цены в карточках
+                priceWrapperList.forEach(elem => {
+                    elem.classList.remove(`table-item__price-wrapper--${exActiveNumber}`)
+                    elem.classList.add(`table-item__price-wrapper--${categoryNumber}`)
+                })
             })
         })
     })
