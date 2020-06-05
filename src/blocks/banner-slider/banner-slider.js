e9883042dcb672086d896aeedd8dac4346449b41
Michael <ko4erov@mail.ru>
2020-06-05 16:44:02 +0300

    banner slider main page ready

--- src/blocks/banner-slider/banner-slider.js
+++ src/blocks/banner-slider/banner-slider.js
@@ -19,33 +19,14 @@ function bannerSlider(attr) {
             autoplay:false, //Автозапуск слайдера
             smartSpeed:1000, //Время движения слайда
             autoplayTimeout:2000, //Время смены слайда
+            autoplay: true, //Автоматическое пролистывание
+            autoplayTimeout: 5000, //Интервалы между пролистыванием элементов
+            autoplayHoverPause: true,//Останавливает автопроигрывание если навести мышкой (hover) на элемент
             items:1,
-            // responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
-            //     0:{
-            //         items:1
-            //     },
-            //     // 600:{
-            //     //     items:2
-            //     // },
-            //     768:{
-            //         items:imgCount
-            //     }
-            // }
+            
         });
 
-        // получаем список точек
-        // const dotsList = elem.querySelectorAll('.owl-dot')
-
-
-        // вставляем в каждую точку по картинке, адрес которой берём у элементов списка картинок
-        // for (let i = 0; i < dotsList.length; i++ ) {
-        //     const imgSrc = imgList[i].getAttribute('src')
-            
-        //     const imgElem = document.createElement('img')
-        //     imgElem.classList.add('owl-dot__img')
-        //     imgElem.setAttribute('src', imgSrc)
-        //     dotsList[i].append(imgElem)
-        // }
+       
 
         
     })
